8a9e766d2f4b974c4ba5809bb046da50c8bf6eee
pranataalam67 <100541557+pranataalam67@users.noreply.github.com>
2022-06-08 10:51:58 +0700

    Update MainActivity.kt

--- MainActivity.kt
+++ MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.newsampleaplication
-mport androidx.appcompat.app.AppCompatActivity
+
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.view.animation.Animation
